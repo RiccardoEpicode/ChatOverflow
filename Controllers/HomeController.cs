542fe7ea168c9613fbdfff097b46652a726ef205
RiccardoEpicode <realiriccardo05@gmail.com>
2026-03-20 13:09:34 +0100

    update

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ChatOverflow.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Home()
+        {
+            return View();
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -2,7 +2,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-builder.Services.AddControllers();
+builder.Services.AddControllersWithViews();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 
@@ -16,8 +16,12 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
+app.UseStaticFiles(); // Standard static files middleware
+
 app.UseAuthorization();
 
-app.MapControllers();
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Home}/{id?}");
 
 app.Run();
