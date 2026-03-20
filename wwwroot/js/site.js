3d1ee38a8b8f6027cbd8d34b041c2f62f2ec5848
RiccardoEpicode <realiriccardo05@gmail.com>
2026-03-20 14:28:39 +0100

    update

---
+++ wwwroot/js/site.js
@@ -0,0 +1,27 @@
+﻿$(function () {
+
+    $('.dropdown-menu a.dropdown-toggle').on('click', function (e) {
+        if (!$(this).next().hasClass('show')) {
+            $(this).parents('.dropdown-menu').first().find('.show').removeClass('show');
+        }
+
+        var $subMenu = $(this).next('.dropdown-menu');
+        $subMenu.toggleClass('show');
+
+        $(this).parents('li.nav-item.dropdown.show').on('hidden.bs.dropdown', function (e) {
+            $('.dropdown-submenu .show').removeClass('show');
+        });
+
+        return false;
+    });
+});
+
+
+//LEAFLET INTERACTIVE MAP
+var map = L.map('map').setView([43.69110094434996, -79.45449628940868], 17);
+var marker = L.marker([43.69110094434996, -79.45449628940868]).addTo(map);
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19
+}).addTo(map);
+
+
