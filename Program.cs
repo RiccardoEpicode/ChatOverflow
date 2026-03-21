95f759fbb7c8be4779b3b76109811fa2049dc922
RiccardoEpicode <realiriccardo05@gmail.com>
2026-03-21 22:05:46 +0100

    update

--- Program.cs
+++ Program.cs
@@ -1,9 +1,19 @@
+using Microsoft.EntityFrameworkCore;
+using ChatOverflow.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
+// Configure SQL Server connection
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("SqlString")));
+
 builder.Services.AddControllersWithViews();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+
+
+
 
 var app = builder.Build();
 
