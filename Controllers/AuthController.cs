bd9660efc263ca53e1571fcebff95879d29d0c7e
RiccardoEpicode <realiriccardo05@gmail.com>
2026-03-21 19:26:38 +0100

    update

---
+++ Controllers/AuthController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ChatOverflow.Controllers
+{
+    public class AuthController : Controller
+    {
+        public IActionResult LoginPage()
+        {
+            return View();
+        }
+        public IActionResult RegisterPage()
+        {
+            return View();
+        }
+    }
+}
