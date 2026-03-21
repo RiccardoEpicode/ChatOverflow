fb456d504924b3a049a2edb653447e7857a78359
RiccardoEpicode <realiriccardo05@gmail.com>
2026-03-21 21:36:19 +0100

    update

--- Program.cs
+++ Program.cs
@@ -4,14 +4,12 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllersWithViews();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.MapOpenApi();
 }
 
 app.UseHttpsRedirection();
